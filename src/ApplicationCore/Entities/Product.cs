00b45a2175086b649588d612c44a997c6c1162f9
Deniz ÇAY <idenizcay@gmail.com>
2021-09-23 10:55:50 +0300

    create entities

---
+++ src/ApplicationCore/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PicturePath { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+
+    }
+}
